7a380afb9fa80a2bdf7ab30dd371eb6cfd7ad9b8
Kartik Lunkad <kartiklunkad@gmail.com>
2016-05-22 21:22:19 -0400

    Update 01-smbfs.sh

--- .profile.d/01-smbfs.sh
+++ .profile.d/01-smbfs.sh
@@ -11,12 +11,7 @@ done
 LD_LIBRARY_PATH=/app/local/usr/lib/x86_64-linux-gnu:/app/local/usr/lib/x86_64-linux-gnu/samba
 export LD_LIBRARY_PATH
 chmod 600 /app/smbnetfs.cf
-#mkdir ~/.smb
-#cp -R ~/app/.smb/* ~/.smb/
-#chmod 600 ~/.smb/smbnetfs.conf
-#chmod 600 ~/.smb/smb.conf
-#chmod 600 ~/.smb/smbnetfs.auth
-/app/local/smbnetfs  /app/mnt -o config=/app/smbnetfs.cf,log_file=/tmp/smbnetfs.log 
+/app/local/smbnetfs  /app/mnt -o config=/app/smbnetfs.cf
 sleep 3
 rm -fr /app/storage
-ln -s /app/mnt/WORKGROUP/FREENAS/CIFSShare /app/storage
+ln -s /app/mnt/10.68.147.253/CIFSShare /app/storage
